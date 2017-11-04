7014a33b1c1e8e86b48a7c8bd6f0c9feb4e3e965
Oleksii Oboianskyi <piva1839@gmail.com>
2017-11-04 21:44:43 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+      var mainNavigation = $(".main-navigation");
+      $(".main-navigation a").click(function(){
+      mainNavigation.hide(500);
+      });
+      });
